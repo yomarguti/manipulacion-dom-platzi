f3ea0b7a656d2a3622f1f3ab113f44a92e1dc428
yomarguti <yomarguti@users.noreply.github.com>
2021-01-20 13:46:19 +0000

    deploy: 5148c03dc8d2bd260e45f944459ae6a53d893b2b

---
+++ _dist_/index.js
@@ -0,0 +1,48 @@
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
+const baseUrl = 'https://platzi-avo.vercel.app';
+
+const appNode = document.querySelector('.app');
+
+window
+  .fetch(`${baseUrl}/api/avo`)
+  .then((res) => res.json())
+  .then((responseJson) => {
+    const todosLosItems = [];
+    responseJson.data.forEach((item) => {
+      const image = document.createElement('img');
+      image.src = `${baseUrl}${item.image}`;
+      image.className = 'rounded-full transition transform hover:-rotate-3';
+
+      const imageContainer = document.createElement('div');
+      imageContainer.className =
+        'h-24 w-24 flex-shrink-0 p-1 rounded-full bg-gradient-to-tr from-green-500 to-indigo-600';
+      imageContainer.append(image);
+
+      const title = document.createElement('h2');
+      title.textContent = item.name;
+      title.className = 'font-bold text-lg mb-2 text-left';
+
+      const price = document.createElement('div');
+      price.textContent = item.price;
+
+      const sku = document.createElement('div');
+      sku.textContent = `SKU: ${item.sku}`;
+      sku.className = 'text-sm';
+
+      const infoContainer = document.createElement('div');
+      infoContainer.append(title, sku, price);
+      infoContainer.className = 'flex flex-col items-start';
+
+      const container = document.createElement('div');
+      container.className =
+        'flex space-x-5 p-5 rounded-md border border-gray-200 shadow-md transition transform hover:scale-105  hover:bg-gray-300 cursor-pointer';
+
+      container.append(imageContainer, infoContainer);
+      todosLosItems.push(container);
+    });
+
+    appNode.append(...todosLosItems);
+  });
